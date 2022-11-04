64e36b0b8bdcf7d4b97260d2385f8d7c79fd9df5
Pauline C <poc215@nyu.edu>
2022-11-04 11:28:41 -0400

    added customize.js

---
+++ public/customize.js
@@ -0,0 +1,163 @@
+
+//CONSTANT
+const userCoral = {
+	species: null, 
+	color: null,
+	name: '',
+};
+
+//MAIN
+const leftContainer = document.getElementById('left');
+const rightContainer = document.getElementById('right');
+
+
+//Species
+
+const coralContainer = document.getElementById('species-container');
+
+const FS = document.getElementById('FSButton');
+const AL = document.getElementById('ALButton');
+const AM = document.getElementById('AMButton');
+
+const FSImg = document.getElementById(CORAL_SPECIES['FUNGIA_SCUTARIA']);
+const AMImg = document.getElementById(CORAL_SPECIES['ACROPORA_MILLEPORA']);
+const ALImg = document.getElementById(CORAL_SPECIES['ACROPORA_LORIPES']);
+
+const onSpeciesClick = (coral, e) => {
+	userCoral.species = CORAL_SPECIES[coral];
+	unselectSpeciesButtons();
+
+	if(e.target.tagName === "IMG"){
+		e.target.parentNode.classList.add('selected');
+	}
+	else if(e.target.tagName === "BUTTON"){
+		e.target.classList.add('selected');
+	}
+
+	let top = document.getElementById('top');
+	if(top){
+		leftContainer.insertBefore(coralContainer,leftContainer.firstChild);
+		leftContainer.classList.remove('hidden');
+		top.remove();
+		rightContainer.classList.remove('hidden');
+	}
+
+	hideAllCoralImgs();
+	const currImg = document.getElementById(userCoral.species);
+	currImg.classList.remove('hidden');
+
+
+};
+
+const unselectSpeciesButtons = () => {
+	FS.classList.remove('selected');
+	AL.classList.remove('selected');
+	AM.classList.remove('selected');
+};
+
+const hideAllCoralImgs = () => {
+	FSImg.classList.add('hidden');
+	ALImg.classList.add('hidden');
+	AMImg.classList.add('hidden');
+}
+
+FS.onclick = onSpeciesClick.bind(this, CORAL_SPECIES['FUNGIA_SCUTARIA']);
+AL.onclick = onSpeciesClick.bind(this, CORAL_SPECIES['ACROPORA_LORIPES']);
+AM.onclick = onSpeciesClick.bind(this, CORAL_SPECIES['ACROPORA_MILLEPORA']);
+
+
+// Colors
+const redButton = document.getElementById('RED');
+const orangeButton = document.getElementById('ORANGE');
+const yellowButton = document.getElementById('YELLOW');
+const greenButton = document.getElementById('GREEN');
+const cyanButton = document.getElementById('CYAN');
+const blueButton = document.getElementById('BLUE');
+const purpleButton = document.getElementById('PURPLE');
+const pinkButton = document.getElementById('PINK');
+
+const onColorClick = (color, e) => {
+	userCoral.color = COLORS[color];
+	unselectColorButtons();
+	e.target.classList.add('selected');
+
+	nameContainer.classList.remove('hidden');
+	submitContainer.classList.remove('hidden');
+};
+
+const unselectColorButtons = () => {
+	redButton.classList.remove('selected');
+	orangeButton.classList.remove('selected');
+	yellowButton.classList.remove('selected');
+	greenButton.classList.remove('selected');
+	cyanButton.classList.remove('selected');
+	blueButton.classList.remove('selected');
+	purpleButton.classList.remove('selected');
+	pinkButton.classList.remove('selected');
+};
+
+redButton.onclick = onColorClick.bind(this, 'RED');
+orangeButton.onclick = onColorClick.bind(this, 'ORANGE');
+yellowButton.onclick = onColorClick.bind(this, 'YELLOW');
+greenButton.onclick = onColorClick.bind(this, 'GREEN');
+cyanButton.onclick = onColorClick.bind(this, 'CYAN');
+blueButton.onclick = onColorClick.bind(this, 'BLUE');
+purpleButton.onclick = onColorClick.bind(this, 'PURPLE');
+pinkButton.onclick = onColorClick.bind(this, 'PINK');
+
+//Name
+
+const nameContainer = document.getElementById('name-container');
+const name = document.getElementById('nameInput');
+const inputError = document.getElementById('inputError');
+
+const hideMobileKeyboardOnReturn = (element) => {
+    element.addEventListener('keyup', (keyboardEvent) => {
+        const key = keyboardEvent.code || keyboardEvent.keyCode;
+        if (key === 'Enter' || key === 13) {
+            element.blur();
+        }
+    });
+};
+
+hideMobileKeyboardOnReturn(name);
+name.addEventListener('input', (e) => {
+
+	if(hasSubmit){
+
+		if(name.value.trim().length > 0 ){
+			inputError.classList.add('hidden');
+		}
+
+		else{
+			inputError.classList.remove('hidden');
+		}
+	}
+});
+
+
+//Submit
+const submitContainer = document.getElementById('submit-container');
+const submit = document.getElementById('submit');
+let hasSubmit = false;
+
+const onSubmitClick = (endCb) => {
+	hasSubmit = true;
+	console.log("Submitting:");
+
+	if(name.value && name.value.trim().length > 0){
+		console.log("Name is valid");
+		userCoral.name = name.value;
+		sendMqttMessage(userCoral);
+		endCb();
+		
+	}
+	else{
+		console.log("Empty name input");
+		inputError.classList.remove('hidden');
+		
+	}
+};
+
+
+//RESET
